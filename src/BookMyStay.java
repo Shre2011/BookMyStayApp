7f80963431cc49823ea17f0c86cd0f23f4d88d8f
Shreyanka <shreyanka.in@gmail.com>
2026-03-24 14:49:34 +0530

    UC2 done

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,10 +1,73 @@
-public class BookMyStay {
+abstract class Room {
+    private int beds;
+    private double price;
+    private String type;
 
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+}
+
+public class BookMyStay {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Hotel Booking System");
-        System.out.println("Application: Hotel Booking System");
-        System.out.println("Version: 1.0");
-        System.out.println("Application started successfully. Exiting now...");
+        Room single = new SingleRoom();
+        Room doubleroom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        System.out.println("Welcome to Book My Stay App\n");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailability + "\n");
+
+        doubleroom.displayDetails();
+        System.out.println("Available: " + doubleAvailability + "\n");
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailability + "\n");
+
+        System.out.println("Application Finished.");
     }
 }
